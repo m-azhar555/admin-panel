d58a8c697ff0baba2be9e9013ffc00570a76bb24
m-azhar555 <ma0024923@gmail.com>
2026-01-30 12:56:38 +0500

    login logic

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,4 +1,5 @@
 const userModel = require("../models/User")
+const bcrypt = require('bcryptjs');
 
 const register = async(req, res)=>{
     try {
@@ -9,8 +10,9 @@ const register = async(req, res)=>{
             return res.status(402).json({success:false,message:'User already exist please login '})
 
         }
+        const hasehedPassword = await bcrypt.hashSync(password,10);
         const newUser = new userModel({
-            username,email,password
+            username,email,password:hasehedPassword
         })
         await newUser.save();
         return res.status(201).json({success:true,message:'User registered successfully',user:newUser })
@@ -19,4 +21,23 @@ const register = async(req, res)=>{
         return res.status(500).json({ success: false, message: "Internal server error" });
     }
 }
-module.exports = register;
+const login = async(req,res)=>{
+    try {
+        const{email,password}=req.body
+        const user = await userModel.findOne({email})
+        if(!user){
+            return res.status(404).json({success:false,message:"User not found please register"})
+
+        }
+        const isVallidpassword = await bcrypt.compare(password,user.password)
+        if(!isVallidpassword){
+            return res.status(400).json({success:false,message:"wrong password"})
+        }
+        return res.status(200).json({success:true,message:"User login successflly",user})
+    } catch (error) {
+        console.log('login error', error)
+        return res.status(400).json({success:false,message:"internl server error"})
+        
+    }
+}
+module.exports = {register,login};
